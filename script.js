ba937152fa6e686ede77f7460e500c8240c18313
MadGrip <madgrip3@gmail.com>
2020-10-26 10:44:20 +0100

    added type effect at home screen

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ const header = document.getElementById("header");
 const home = document.getElementById("home");
 const observerHome = new IntersectionObserver(function(entries, observerHome){
     entries.forEach(entry => {
-        console.log(entry);
         if(!entry.isIntersecting){
             header.classList.add("header--scrolled");
         }else if(entry.isIntersecting){
             header.classList.remove("header--scrolled");
-            console.log("removed");
         }
     })
 }, {
@@ -21,15 +19,74 @@ observerHome.observe(home);
 const faServicesIcons = Array.from(document.querySelectorAll(".fa-3x"));
 const faSocial = Array.from(document.querySelectorAll(".fa-facebook-square, .fa-instagram-square, .fa-linkedin"));
 
-console.log(screen.width);
-console.log(faSocial);
-
 if(screen.width <= 425){
     faServicesIcons.forEach(icon => {
         icon.classList.remove("fa-3x");
         icon.classList.add("fa-2x");
     });
-
-    
-
 }
+
+/* XXX Home text animation */
+class TypeWriter{
+    constructor(txtElement, words, wait = 3000){
+        this.txtElement = txtElement; //span
+        this.words = words;
+        this.wordIndex = 0;
+        this.txt = ''; //current text
+        this.wait = parseInt(wait, 10); //wait time at the end
+        this.isDeleting = false;
+        this.type(); //method that types text
+    }
+
+    type(){
+        //Index of current word
+        const current = this.wordIndex % this.words.length;
+        //Current word that is typed in
+        const fullTxt = this.words[current];
+
+      // YYY Adding and removing characters
+        if(this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+      // YYY Insert txt into element
+        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+      // YYY Initial Type Speed
+        let typeSpeed = 100;
+        if(this.isDeleting) {
+        typeSpeed /= 2;
+        }
+
+      // YYY If word is complete
+        if(!this.isDeleting && this.txt === fullTxt){
+            // Make pause at end
+            typeSpeed = this.wait;
+            // Set delete to true
+            this.isDeleting = true;
+        } else if(this.isDeleting && this.txt === ''){
+            this.isDeleting = false;
+            // Move to next word
+            this.wordIndex++;
+            // Pause before start typing
+            typeSpeed = 500;
+        }
+
+        setTimeout(() => this.type(), typeSpeed);
+    }
+}
+
+
+  // YYY Init On DOM Load
+    document.addEventListener('DOMContentLoaded', init);
+
+  // YYY Init App
+    function init() {
+        const txtElement = document.querySelector('.type-effect');
+        const wait = txtElement.getAttribute('data-wait');
+        const words = JSON.parse(txtElement.getAttribute('data-words'));
+        // Init TypeWriter
+        new TypeWriter(txtElement, words, wait);
+    }
